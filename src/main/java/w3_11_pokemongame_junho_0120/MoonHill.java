20119133e7980909668f9cbfd2572a62f0b6560d
junho526 <jungjunho0526@naver.com>
2025-01-27 10:19:11 +0900

    test

--- src/main/java/w3_11_pokemongame_junho_0120/MoonHill.java
+++ src/main/java/w3_11_pokemongame_junho_0120/MoonHill.java
@@ -2,6 +2,7 @@ package w3_11_pokemongame_junho_0120;
 
 public class MoonHill implements MapLocation{
 
+    //
     @Override
     public void enter(Trainer trainer, Trainer enemyTrainer, Pokemon wildPokemon) {
 
